161139f4e282662384c0cacdf4d0de68dbff3813
JekinMoon <moonjekin@gmail.com>
2026-02-10 14:32:09 +0900

    수정

--- Web/04_react/06-routing/src/index.js
+++ Web/04_react/06-routing/src/index.js
@@ -2,13 +2,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
-// import Practice1 from './Practice1';
+import Practice1 from './Practice1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter >
     <App />
-    {/* <Practice1 /> */}
+    <Practice1 />
   </BrowserRouter>
   
 );
